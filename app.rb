36fbc5d0bb4bcfb6c84fc9cfe14eb173878d9d6f
chrisfarms <chris.farmiloe@digital.cabinet-office.gov.uk>
2017-10-17 16:55:09 +0100

    fixup! get form path working without js

--- app.rb
+++ app.rb
@@ -1,6 +1,7 @@
 require 'sinatra/base'
 require 'sinatra/reloader'
 require 'sinatra/content_for'
+require 'sinatra/multi_route'
 require 'staccato'
 require 'sprockets'
 require 'sprockets-helpers'
@@ -12,6 +13,7 @@ class App < Sinatra::Base
 
 	set :sprockets, Sprockets::Environment.new
 	set :title, 'Gov.UK Platform as a Service'
+	enable :sessions
 
 	configure do
 		sprockets.append_path File.join(root, 'node_modules', 'govuk_frontend_toolkit', 'stylesheets')
@@ -26,13 +28,14 @@ class App < Sinatra::Base
 			config.debug       = true if development?
 		end
 		register Sinatra::Reloader if development?
+		register Sinatra::MultiRoute
 	end
 
 	helpers Sinatra::ContentFor
 	helpers Sprockets::Helpers
 
 	after do
-		headers 'X-Robots-Tag' => 'noindex, nofollow'
+		headers 'X-Robots-Tag' => 'noindex, nofollow' #FIXME
 	end
 
 	get '/?' do
@@ -92,6 +95,27 @@ class App < Sinatra::Base
 		settings.sprockets.call(env)
 	end
 
+	route :get, :post, '/request-account' do
+		puts "params: #{params}"
+		set_from_param(:person_name)
+		set_from_param(:person_email)
+		set_from_param(:person_is_manager)
+		set_from_param(:department_name)
+		set_from_param(:service_name)
+		(params[:invites] || {}).each do |k,v|
+			invs = session[:invites] ||= {}
+			invs ||= {}
+			invs[k] = v
+			invs[k][:idx] = k.to_i
+		end
+		puts "session: #{session.to_h}"
+		if params[:continue] == 'false'
+			erb :thanks
+		else
+			erb :'request-account'
+		end
+	end
+
 	get '/*' do
 		viewname = params[:splat].first
 		if File.exist?("views/#{viewname}.erb")
@@ -112,6 +136,29 @@ class App < Sinatra::Base
 
 	helpers do
 
+		# the current session account details
+		def account
+			session[:account] ||= {}
+		end
+
+		# the current active invite from session
+		def invite
+			invites = session[:invites] ||= {}
+			idx = (params[:invite] || '0')
+			invites[idx] ||= {:idx => idx.to_i}
+		end
+
+		def has_next_invite?
+			idx = (params[:invite] || '0').to_i+1
+			invites = session[:invites] ||= {}
+			invites.has_key? idx.to_s
+		end
+
+		def set_from_param(name)
+			if params[name].is_a?(String) && !params[name].empty?
+				account[name] = params[name]
+			end
+		end
 
 		# return comma seperated list of errors from validation if resourse has been validated
 		def errors_for(resource, field)
--- assets/js/application.js
+++ assets/js/application.js
@@ -1,7 +1,2 @@
 //= require header.js
 
-jQuery(function($){
-    $(".request-trial-form").change(function(e) {
-        $(".request-trial-form").attr('action', e.target.value)
-    });
-});
